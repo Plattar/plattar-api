864095997a4f7932de0832270d7289cfdf1a14e4
David Arayan <david.arayan12@gmail.com>
2020-08-21 17:01:24 +1000

    - Added options to get the full remote path of files

--- plattar-api/types/file/file-base.js
+++ plattar-api/types/file/file-base.js
@@ -10,6 +10,30 @@ class FileBase extends PlattarBase {
             throw new Error('FileBase is abstract and cannot be created');
         }
     }
+
+    /**
+     * Returns the full remote path of the file
+     * Use this path to download the object into your machine
+     */
+    get filePath() {
+        if (!this.attributes.path) {
+            return undefined;
+        }
+
+        return this._query.server.cdn + this.attributes.path + this.attributes.original_filename;
+    }
+
+    /**
+     * Returns the full remote path of the file backup as a Zip File
+     * Use this path to download the object object into your machine
+     */
+    get fileBackupPath() {
+        if (!this.attributes.path) {
+            return undefined;
+        }
+
+        return this._query.server.cdn + this.attributes.path + this.attributes.original_upload;
+    }
 }
 
 module.exports = FileBase;
