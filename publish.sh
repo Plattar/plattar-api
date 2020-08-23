ae5e3e55df83e80464f751c4906bb3e70a5da3e9
David Arayan <david.arayan12@gmail.com>
2020-08-24 09:43:25 +1000

    - Fixed publish script

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@ rm -rf plattar-api/README.md
 rm -rf plattar-api/graphics
 cp README.md plattar-api/README.md
 cp -R graphics plattar-api/
-#cd plattar-api && npm publish --scope=public && cd ../
+cd plattar-api && npm publish --scope=public && cd ../
 rm -rf plattar-api/README.md
 rm -rf plattar-api/graphics
